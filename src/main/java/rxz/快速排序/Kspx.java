ff47edab679b56eaa1ff7f24a5d88fdb2368f382
Mengjianmingri <rxz7250@163.com>
2020-03-07 10:07:45 +0800

    快速排序时空复杂度总结及校数器实现

--- src/main/java/rxz/快速排序/Kspx.java
+++ src/main/java/rxz/快速排序/Kspx.java
@@ -1,5 +1,12 @@
 package rxz.快速排序;
 
+/**
+ * TODO:快速排序算法实现
+ * 时间复杂度:平均:O(N*logN)
+ *          最坏:O(N^2)(此时第一次的轴为最大或最小,为了避免这种情况
+ *                  可以先在数组中随机取一值与array.length-1处的数据互换再使用快排,减小概率)
+ * 空间复杂度:O(logN)
+ */
 public class Kspx {
     public static void solve(int[] array, int left, int right) {
         if (left > right) {
@@ -10,7 +17,7 @@ public class Kspx {
         solve(array, shaftP + 1, right);
     }
 
-    public static int sort1(int[] array, int left, int right) {//双轴快排(未递归),未优化,纯脑壳怎么想手怎么敲,可实现
+    public static int sort1(int[] array, int left, int right) {//双轴快排(未递归),未优化,纯怎么想手怎么敲,可实现,语句臃肿
         if (left >= right) {
             return left;
         }
---
+++ src/test/java/对数器/Check.java
@@ -0,0 +1,42 @@
+package 对数器;
+
+import org.junit.Before;
+import org.junit.Test;
+import rxz.冒泡排序.Mppx;
+import rxz.快速排序.Kspx;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * TODO:通过和jdk的sort方法检验自己的排序算法的正确性
+ */
+public class Check {
+    int[] array = new int[10000];
+    int[] array1 = new int[10000];
+
+    @Before
+    public void pre() {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10000);
+        }
+        System.arraycopy(array, 0, array1, 0, array.length);
+    }
+
+    @Test
+    public void check() {
+        boolean flag = true;
+        Arrays.sort(array);
+        Kspx.solve(array1, 0, array1.length - 1);
+        //Mppx.solve(array1);
+
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != array[i]) {
+                flag = false;
+                break;
+            }
+        }
+        System.out.println(flag);
+    }
+}
